f6d5d9a55646cace074fdac376726f88f2639474
Meir Neustadt <msn.binah.1@gmail.com>
2024-02-12 13:24:14 +0200

    Update ProfitFinder.py

--- ProfitFinder.py
+++ ProfitFinder.py
@@ -1,4 +1,4 @@
-def S(a):
+def S1(a):
     Buyday=0
     SellDay=0
     Profit=0
@@ -8,7 +8,30 @@ def S(a):
                 BuyDay=i
                 SellDay=j
                 Profit=(a[j]-a[i])
-    return(BuyDay,SellDay,Bruto)            
+    return(BuyDay,SellDay,Bruto) 
 
-a=[3,5,1,7,4,9,6,4]
-print(S(a))
+def S2(a):
+    Buyday=0
+    SellDay=0
+    MostExpensive,ExpensiveDay=Expensive(a,0)
+    Bruto=0
+    for i in range(len(a)):
+        if ExpensiveDay < i:
+            MostExpensive,ExpensiveDay=Expensive(a,i+1)
+        if (MostExpensive-a[i])>Bruto:
+            Bruto=(MostExpensive-a[i])
+            Buyday=i
+            SellDay=ExpensiveDay   
+    return(Buyday,SellDay,Bruto)
+
+def Expensive(a,i):
+    ME=a[i]
+    Day=i
+    for i in range(i+1,len(a)):
+        if a[i]>=ME:
+           ME=a[i]
+           Day=i                       
+    return (ME,Day)
+
+a=[3,6,4,1,8,9,4,0,3]
+print(S1(a),S2(a))
